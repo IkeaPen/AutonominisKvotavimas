cd2a50fc4de2a1de6bc80691561e1c30e0ff65f7
IkeaPen <agne8rimk@gmail.com>
2025-03-05 14:23:50 +0200

    Made 'pages' and 'components' folders and set up routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,13 @@
-import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [getValue, setGetValue] = useState("");
-
-
-  useEffect(() => {
-    fetch("/api/test").then((_res) => {
-      if (_res.status === 200) {
-        _res.text().then((_text: string) => {
-          setGetValue(_text);
-        });
-      } else {
-        setGetValue("Express server is not working.")
-      }
-    });
-  }, []);
-  
   return(
-    <>
-      <h1>Test</h1>
-      <h2>{getValue}</h2>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 
 }
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+function HomePage() {
+  const [getValue, setGetValue] = useState("");
+
+  
+  useEffect(() => {
+    fetch("/api/test").then((_res) => {
+      if (_res.status === 200) {
+        _res.text().then((_text: string) => {
+          setGetValue(_text);
+        });
+      } else {
+        setGetValue("Express server is not working.")
+      }
+    });
+  }, []);
+  
+  return(
+    <>
+      <h1>Test</h1>
+      <h2>{getValue}</h2>
+    </>
+  );
+
+}
+
+export default HomePage;
